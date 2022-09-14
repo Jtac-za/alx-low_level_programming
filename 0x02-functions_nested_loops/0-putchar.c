7f1ce5add654327a5a4b642c7d7ef05aaed3887e
Jtac-za <jreecethomas@gmail.com>
2022-09-14 03:13:09 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,22 @@
 #include <unistd.h>
-#include <sys/syscall.h>
 #include "main.h"
+
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 int main(void)
 {
-	const char msg[] = "_putchar\n";
-
-	syscall(SYS_write, 1, msg, sizeof(msg));
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 return (0);
 }
