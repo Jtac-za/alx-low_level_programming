9b7383eecfdb4b608a017d03d1434d562a3d260d
Jtac-za <jreecethomas@gmail.com>
2022-09-14 02:00:05 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		putchar(n);
+
+	}
+	putchar('\n');
+return (0);
+}
