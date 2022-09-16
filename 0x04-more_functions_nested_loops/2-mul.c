0811ba1e142b62586087cef50222c3a0068deca3
Jtac-za <jreecethomas@gmail.com>
2022-09-16 04:55:15 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - main entry
+ * @a: integer to be checked
+ * @b: integer to be checked
+ *
+ * Return: multiply by two integers
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
