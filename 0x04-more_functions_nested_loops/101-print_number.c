2207cef40895cfb7908ccffd367c1508e02cadc3
Jtac-za <jreecethomas@gmail.com>
2022-09-16 11:21:02 -0700

    commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,21 +7,18 @@
  *
  * Return: void
  */
-void print_numbers(int n)
+void print_number(int n)
 {
-	unsigned int k = n;
+	unsinged; int num = n;
 
 	if (n < 0)
 	{
-		n *= -1;
-		k = n;
 		_putchar('-');
+		num = -num;
 	}
 
-	k /= 10;
+	if ((num / 10 > 0))
+			print_number(num / 10);
 
-	if (k != 0)
-		print_numbers(k);
-
-	_putchar((unsigned int) n % 10 + '0');
+	_putchar((num % 10) + '0');
 }
