aed0e9fb6ed1f0008388d3204d1ca141e6b0ea7d
Jtac-za <jreecethomas@gmail.com>
2022-09-16 05:18:58 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - main entry
+ *
+ * Description: prints numbers form 0 to 9 except 2 and 4
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		if (n != 50 && n != 52)
+			_putchar(n);
+	}
+	_putchar('\n');
+}
