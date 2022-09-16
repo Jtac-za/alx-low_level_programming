ec7ce218ab6a946cc105af27a6cb797b87d9803f
Jtac-za <jreecethomas@gmail.com>
2022-09-16 05:02:02 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - main entry
+ * void: integer to be checked
+ *
+ * Return:
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	_putchar(n);
+
+	_putchar('\n');
+}	
